4d391cc89aefa9d9e4b575efce40771820e96c47
gremble0 <hermanstornes@gmail.com>
2024-08-16 11:06:25 +0200

    Finish diagram and tests

--- src/test/java/CarTest.java
+++ src/test/java/CarTest.java
@@ -27,4 +27,53 @@ class CarTest {
     Car car = new Car("green", new Battery(false), new Controls(true));
     Assertions.assertTrue(car.getControls().isAdvanced());
   }
+
+  @Test
+  public void testCarBatteryCharge() {
+    Car car = new Car("green", new Battery(false), new Controls(false));
+    Assertions.assertEquals(100, car.getBattery().getRemainingCharge());
+  }
+
+  @Test
+  public void testCarDrive() {
+    Car car = new Car("green", new Battery(false), new Controls(false));
+    Location start = car.getLocation();
+    car.getControls().setDirection(Direction.Right);
+    car.getControls().drive(100, car.getLocation());
+    Assertions.assertEquals(start.x + 100, car.getLocation().x);
+    Assertions.assertEquals(start.y, car.getLocation().y);
+    Assertions.assertEquals(start.z, car.getLocation().z);
+  }
+
+  @Test
+  public void testCarBrakes() {
+    Car car = new Car("green", new Battery(false), new Controls(false));
+    Location start = car.getLocation();
+    car.getControls().brake();
+    Assertions.assertFalse(car.getControls().isMoving());
+    Assertions.assertEquals(start.x, car.getLocation().x);
+    Assertions.assertEquals(start.y, car.getLocation().y);
+    Assertions.assertEquals(start.z, car.getLocation().z);
+  }
+
+  @Test
+  public void testCarTurning() {
+    Car car = new Car("green", new Battery(false), new Controls(false));
+    car.getControls().turnLeft();
+    Assertions.assertEquals(car.getControls().getDirection(), Direction.Left);
+    car.getControls().turnLeft();
+    Assertions.assertEquals(car.getControls().getDirection(), Direction.Down);
+    car.getControls().turnRight();
+    Assertions.assertEquals(car.getControls().getDirection(), Direction.Left);
+  }
+
+  @Test
+  public void testCarReplaceBattery() {
+    Battery oldBattery = new Battery(false);
+    Battery newBattery = new Battery(false);
+    Car car = new Car("green", oldBattery, new Controls(false));
+    Assertions.assertEquals(car.getBattery(), oldBattery);
+    car.replaceBattery(newBattery);
+    Assertions.assertEquals(car.getBattery(), newBattery);
+  }
 }
